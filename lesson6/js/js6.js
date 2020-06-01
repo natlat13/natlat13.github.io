dbe561ec85cdc6cce081a1f284517cef87cab616
natlat13 <lat10002@byui.edu>
2020-06-01 13:35:12 -0600

    sdc

--- lesson6/js/js6.js
+++ lesson6/js/js6.js
@@ -44,11 +44,3 @@ function windChill(tempF, speed) {
 }
 document.getElementById("windchill").innerHTML = chill;
 
-//Font loader
-WebFont.load({
-    google: {
-      families: [
-         'Playfair Display, Open Sans Condensed'
-      ]
-    }
-  });
