1d879f2454d781bfdf5485fbcbc3321fd5b865eb
natlat13 <lat10002@byui.edu>
2020-06-08 12:45:23 -0600

    ahh

---
+++ lesson7/js/js7.js
@@ -0,0 +1,32 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+
+  
+  function toggleMenu () {
+    document.getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
+}
+
+document.getElementById('year').textContent = new Date().getFullYear();
+
+var d = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("day").innerHTML = days[d.getDay()];
+
+var date = new Date();
+document.getElementById("date").innerHTML = date.getDate();
+
+var m = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+    "November", "December"
+];
+document.getElementById("month").innerHTML = months[m.getMonth()];
