63a8826a142076cbcc77d8a0f8623cdb94019f39
natlat13 <lat10002@byui.edu>
2020-07-23 12:18:33 -0600

    tours

---
+++ finalproject/javascript/tours.js
@@ -0,0 +1,36 @@
+// Nav Bar Ham Button
+function toggleMenu() {
+    document.getElementsByClassName('navigation')[0]
+        .classList.toggle('responsive');
+}
+
+//Automatic Slideshow
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+  }
+
+  //Footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+var d = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("day").innerHTML = days[d.getDay()];
+
+var date = new Date();
+document.getElementById("date").innerHTML = date.getDate();
+
+var m = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+    "November", "December"
+];
+document.getElementById("month").innerHTML = months[m.getMonth()]; 
