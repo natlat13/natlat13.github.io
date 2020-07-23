a23b05d70119947f916afa3fe36bcb43cccc407d
natlat13 <lat10002@byui.edu>
2020-07-22 19:37:18 -0600

    scss

--- finalproject/javascript/index.js
+++ finalproject/javascript/index.js
@@ -1,8 +1,45 @@
 //JSON Pricing
-const requestURL("https://natlat13.github.io/finalproject/json.html");
+const requestURL = "https://natlat13.github.io/finalproject/json/index.json";
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject){
 console.log(jsonObject);
+const pricing = jsonObject['pricing']
+});
 
+/* Nav Bar Ham Button*/
+function toggleMenu() {
+    document.getElementsByClassName('navigation')[0]
+        .classList.toggle('responsive');
+}
 
+//Automatic Slideshow
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+  }
+
+  //Current Weather Section
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=20.5083&lon=-86.9458&units=imperial&APPID=f10fe0a4423c6d05fbdfb9677e974545";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        document.getElementById('currently').textContent = jsonObject.weather[0].description;
+        document.getElementById('temp').innerHTML = `${jsonObject.main.temp}&deg;F`;
+        document.getElementById('humidity').textContent = jsonObject.main.humidity + "%";
+    });
 
 //Footer
 document.getElementById('year').textContent = new Date().getFullYear();
